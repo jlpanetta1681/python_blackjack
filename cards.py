d036256405f0d2ddaa8da10a93e40aa842d7a7b2
Joseph Panetta <jlpanetta1681@icloud.com>
2021-10-09 10:06:43 -0400

    card class and deck class done and tested and working

--- cards.py
+++ cards.py
@@ -1,2 +1,33 @@
 print("Welcome to BlackJack")
-print("Tell me you name.")
+# create the classes
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.val = val
+    def show(self):
+        print ("{} of {}".format(self.val, self.suit))
+    
+card = Card("Spades", 9)
+card.show()
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Hearts", "Spades", "Diamonds", "Clubs"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s,v))
+    
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+deck = Deck()
+deck.show()
+
+
+class Player(object):
+    def __init__(self):
+        pass
